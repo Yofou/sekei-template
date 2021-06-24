804a7005735df9b20dcf30c91f4f92aada07c124
Nathan <39814328+Yofou@users.noreply.github.com>
2021-06-24 05:05:21 +0100

    [📦] added comments & more robust fetching method
    moved fetch logic inside the load hook, this is because if we fetch outside we need to keep track of they port number.

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -1,9 +1,28 @@
-<script>
-    const getPopulation = async () => {
-        return fetch('http://localhost:3000/population.json').then( res => res.json() ) 
+<script context="module">
+    // check out https://kit.svelte.dev/docs#loading for how load behaves
+    // and check out https://kit.svelte.dev/docs#loading-input-fetch
+    // for why we fetching inside load and not in our script tag.
+    export async function load({ fetch }) {
+        // this fetches data from our endpoint and then passes it as a component properity
+        // also notice we don't await the fetch, if we did this would cause
+        // svelte-kit to wait for the response (from our endpoint) and then LOAD the page.
+        return {
+            props: {
+                population: fetch('population.json').then( res => res.json() )
+            }
+        }
     }
 </script>
 
-{#await getPopulation() then response}
+<script>
+    // this defines population properity on index.svelte (a svelte component that serves as a page)
+    // check https://svelte.dev/tutorial/declaring-props for more information
+    export let population;
+</script>
+ 
+<!-- as we don't resolve population in they load hook. 
+     we have passed down a Promise that need's to be resolve here
+-->
+{#await population then response}
     <h1 class="text-2xl">{response.total}</h1>
 {/await}
--- src/routes/population.json.js
+++ src/routes/population.json.js
@@ -1,12 +1,17 @@
 
 // suggest implementing a simple cache so your not requesting external api per user
 
+// checkout https://kit.svelte.dev/docs#routing-endpoints 
+// for more information on what we have acces to here
+// but the just just is, we can create endpoints super easily by exporting
+// simple CRUD operations like get (what we use here), post, del (delete), put, etc....
 /**
  * @type {import('@sveltejs/kit').RequestHandler}
  */
 export async function get() {
     const URI = 'https://www12.statcan.gc.ca/rest/census-recensement/CPR2016.json?dguid=2016A000235&topic=13'
     const response = await fetch(URI).then( res => res.json() )
+    
     if (response.DATA.length === 0) return {
         body: {
           error: 'no data found'  
@@ -14,7 +19,6 @@ export async function get() {
     }
 
     const totalPopulationIndex = response.COLUMNS.indexOf('T_DATA_DONNEE')
-
     return {
         body: {
             total: response.DATA[0][totalPopulationIndex]
